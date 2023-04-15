996b798d0308f96efbdf416df4ac5e5bdee97f55
hertogp <git.hertogp@gmail.com>
2023-04-15 11:11:36 +0200

    puts elixir test macro in outline

--- lua/pdh/outline.lua
+++ lua/pdh/outline.lua
@@ -24,11 +24,13 @@ M.queries = {
     (((comment) @c (#lua-match? @c "^[%s#]+%[%[[^\n]+%]%]")) (#join! "head" "" "[c] " @c))
     ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "def")) (#join! "head" "" "[f] " @a))
     ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "defp")) (#join! "head" "" "[p] " @a))
+    ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "test")) (#join! "head" "" "[t] " @a))
     ((call target: (((identifier) ((arguments) @a)) @x)(#any-of? @x "defguard" "defguardp")) (#join! "head" "" "[g] " @a))
     ((call target: (((identifier) ((arguments) @a)) @x)(#any-of? @x "defmodule" "alias")) (#join! "head" "" "[M] " @x " " @a))
     ((((unary_operator (call (((identifier) @i)(#not-any-of? @i "doc" "spec" "typedoc" "moduledoc")))) @m))(#join! "head" "" "[@] " @m))
     ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "defimpl")) (#join! "head" "" "[I] " @a))
     ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "defstruct")) (#join! "head" "" "[S] " @a))
+    ((call target: (((identifier) ((arguments) @a)) @x)(#eq? @x "use")) (#join! "head" "" "[U] " @a))
   ]],
 
   -- (((unary_operator (call (identifier) @h)) @head) (#not-any-of? @h "spec" "doc" "moduledoc"))
